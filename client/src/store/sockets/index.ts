0ffe6df7d84728fd9e96e7298bf3302913fc0f97
Phil Crossland <phil@thirdwheelstudios.com>
2022-10-09 21:11:18 +0100

    Added isConnected getter

--- client/src/store/sockets/index.ts
+++ client/src/store/sockets/index.ts
@@ -4,12 +4,16 @@ import { io, Socket } from 'socket.io-client'
 export const useSocketsStore = defineStore('sockets', {
   state: () => {
     let _socket: Socket | undefined
+    let _connected: boolean = false
 
-    return { _socket }
+    return { _socket, _connected }
   },
   getters: {
     id(state) {
       return state._socket?.id
+    },
+    isConnected(state) {
+      return state._connected
     },
   },
   actions: {
@@ -18,10 +22,12 @@ export const useSocketsStore = defineStore('sockets', {
 
       socket.on('connect', () => {
         console.log(`connect: ${socket.id}`)
+        this._connected = true
       })
 
       socket.on('disconnect', () => {
         console.log(`disconnect: ${socket.id}`)
+        this._connected = false
       })
 
       this._socket = socket
