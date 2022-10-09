090158e8ff25951854d8db551767661b7fc1cb26
Phil Crossland <phil@thirdwheelstudios.com>
2022-10-09 20:59:53 +0100

    Viewer markup

--- client/src/views/StreamViewerView.vue
+++ client/src/views/StreamViewerView.vue
@@ -1,1 +1,13 @@
-<template>Viewer</template>
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useSocketsStore } from '../store'
+
+const socketsStore = useSocketsStore()
+
+const socketId = computed(() => socketsStore.id)
+</script>
+
+<template>
+  <h1>Viewer</h1>
+  <div>Socket Id: {{ socketId }}</div>
+</template>
