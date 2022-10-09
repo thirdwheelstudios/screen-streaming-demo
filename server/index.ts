9855163d1aeb4e47210f77516c092e9fdd2afa96
Phil Crossland <phil@thirdwheelstudios.com>
2022-10-09 18:43:01 +0100

    added another origin

--- server/index.ts
+++ server/index.ts
@@ -6,7 +6,7 @@ const port = 8080
 const httpServer = createServer()
 const io = new Server(httpServer, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
   },
 })
 
