c69bf39f8259c9cada9738ffa2468afefdfa102b
Phil Crossland <phil@thirdwheelstudios.com>
2022-10-11 22:29:02 +0100

    Deleted composable

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import { webSockets } from './composables/webSockets'
+import { onMounted } from 'vue'
+import { useSocketsStore } from './store'
 
-webSockets()
+const socketsStore = useSocketsStore()
+
+onMounted(() => {
+  socketsStore.connect()
+})
 </script>
 
 <template>
